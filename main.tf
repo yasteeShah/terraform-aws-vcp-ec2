15f78903b2bc13f71820ebd8aeb71a1442616977
yasteeshah <yasteeshah1998@gmail.com>
2025-08-28 22:56:21 -0400

    Adding ec2 module

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
   tags                 = var.tags
 }
+
+module "web" {
+  source        = "./modules/ec2"
+  name          = "${var.name_prefix}-web"
+  vpc_id        = module.vpc.vpc_id
+  subnet_id     = module.vpc.public_subnet_ids[0]
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  tags          = var.tags
+}
---
+++ modules/ec2/outputs.tf
@@ -0,0 +1,11 @@
+output "instance_id" {
+  value = aws_instance.this.id
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
+
+output "security_group_id" {
+  value = aws_security_group.web.id
+}
